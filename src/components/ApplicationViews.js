309326d9dfe327b81acacd4a0cdd0b8b5de9e7b3
Shawn Conerton <conerton52@gmail.com>
2021-02-01 15:55:57 -0600

    end of the day push. post deatil page renderes. the post author is in progress.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { PostDataProvider } from "./posts/PostDataProvider";
+import { PostDetails } from "./posts/PostDetails";
 import { PostList } from "./posts/PostList";
 
 export const ApplicationViews = (props) => {
@@ -12,7 +13,13 @@ export const ApplicationViews = (props) => {
             exact
             path="/posts"
             render={(props) => <PostList {...props} />}
-          ></Route>
+          />
+          <Route
+            path="/posts/:id(\d+)"
+            render={(props) => {
+              return <PostDetails {...props} />;
+            }}
+          />
         </PostDataProvider>
       </div>
     </>
