3255813e6407e6f19187ce4f2c41f85eed48584c
Kristen Steele <ksteelephoto@gmail.com>
2021-02-01 20:11:38 -0600

    import and route PostForm to /posts/create

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { PostDataProvider } from "./posts/PostDataProvider";
 import { PostList } from "./posts/PostList";
+import { PostForm } from "./posts/PostForm";
 
 export const ApplicationViews = (props) => {
   return (
     <>
       <div>
         <PostDataProvider>
-          <Route
-            exact
-            path="/posts"
-            render={(props) => <PostList {...props} />}
-          ></Route>
+          <Route exact path="/posts" render={
+              (props) => <PostList {...props} />
+            } />
+          <Route exact path="/posts/create" render={
+              props => <PostForm {...props} />
+          } />
         </PostDataProvider>
       </div>
     </>
